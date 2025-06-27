704a886b9640b1fc20031ea253adfcf52cfa4f64
Nelexstar <kbartlewski5@gmail.com>
2025-06-27 15:52:14 +0200

    8

---
+++ d.py
@@ -0,0 +1,24 @@
+def beats(n, x, y):
+    if x == 0:
+        return y == n - 1
+    if x == n - 1:
+        return y != 0
+    return x > y
+ 
+for _ in range(int(input())):
+    n = int(input())
+    owner = input()
+    good = False
+    for i in range(n):
+        if owner[i] != 'A':
+            continue
+        good_move = True
+        for j in range(n):
+            if owner[j] == 'B' and beats(n, j, i):
+                good_move = False
+        if good_move:
+            good = True
+    if good:
+        print('Alice')
+    else:
+        print('Bob')
---
+++ n.py
@@ -0,0 +1,22 @@
+def solve():
+    n, k = map(int, input().split(" "))
+
+        
+    r = []
+
+    if n > 2:
+        if k % 2 == 0:
+            for _ in range(n-2): r.append(n-1)
+        else:
+            for _ in range(n-2): r.append(n)
+
+    #n-1
+    r.append(n)
+    r.append(n-1)
+
+    print(" ".join(list(map(str, r))))
+
+
+for _ in range(int(input())):
+    solve()
+        
