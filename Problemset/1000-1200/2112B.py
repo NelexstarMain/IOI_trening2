59202a6d3e6360ef8e5dcbb7370ed8ef8c70a31f
Nelexstar <kbartlewski5@gmail.com>
2025-07-10 12:12:54 +0200

    16

--- Problemset/1000-1200/2112B.py
+++ Problemset/1000-1200/2112B.py
@@ -1,7 +1,26 @@
 for _ in range(int(input())):
     n = int(input())
-
-    a = list(map(int, input().split(" ")))
+    a = list(map(int, input().split()))
 
     if n == 2:
-        print("")
+        if abs(a[1] - a[0]) > 1:
+            print("NO")
+        else:
+            print("YES")
+        continue
+
+    f = a[0]
+    for i in range(1, n):
+        s = a[i]
+        if abs(s - f) <= 1:
+            print("YES")
+            break
+        elif i + 1 < n and min(f, s) <= a[i + 1] <= max(f, s):
+            print("YES")
+            break
+        elif i - 1 >= 0 and min(f, s) <= a[i - 1] <= max(f, s):
+            print("YES")
+            break
+    else:
+        print("NO")
+
--- spy.py
+++ spy.py
@@ -10,4 +10,3 @@ for _ in range(int(input())):
 
 
     
-
