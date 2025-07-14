7a67f6b80f3a0d7a40ce108de1e1fd6b67954f33
Nelexstar <kbartlewski5@gmail.com>
2025-07-14 13:05:53 +0200

    27

--- Problemset/cses/Number_Spiral.py
+++ Problemset/cses/Number_Spiral.py
@@ -1,4 +1,24 @@
 for _  in range(int(input())):
-    n = int(input())
+    y, x = map(int, input().split(" "))
 
+    r = 0
+    x_num = x**2 if x%2 == 1 else (x-1)**2 + 1
+    y_num = y**2 if y%2 == 0 else (y-1)**2 + 1
+
+    if x > y:
+        if x % 2 == 0:
+            r = x_num + (y - 1)
+        else:
+            r = x_num - (y - 1)
     
+    elif y > x:
+        if y % 2 == 0:
+            r = y_num - (x - 1)
+        else:
+            r = y_num + (x - 1)
+        
+    elif x == y:
+        n = x-1
+        r = n*(n+1) + 1
+
+    print(r)
