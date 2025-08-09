6e40fcfbb8c467ef037a7979a74e0f25b4c23534
Nelexstar <kbartlewski5@gmail.com>
2025-08-09 20:46:21 +0200

    2113C

--- Problemset/Codeforces/1700-2000/2113C.cpp
+++ Problemset/Codeforces/1700-2000/2113C.cpp
@@ -1,60 +1,70 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
 #include <string>
-#include <algorithm>
 #include <map>
 #include <set>
-#include <queue>
-#include <stack>
+#include <unordered_map>
+#include <random>
+#include <chrono>
+#include <cassert>
 #include <numeric>
+#include <bitset>
+#include <iomanip>
+#include <queue>
+#include <unordered_set>
+#include <fstream>
+#include <random>
 
 using namespace std;
 
 using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
 
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i, a, b) for(int i = a; i <= b; ++i)
-#define FWD(i, a, b) for(int i = a; i < b; ++i)
-#define REV(i, a, b) for(int i = a; i >= b; --i)
-#define ALL(a) (a).begin(), (a).end()
-#define SIZE(a) (int)((a).size())
+mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
+const int MAXN = 500;
+int sum[MAXN + 1][MAXN + 1];
 
-const int INF = 1e9 + 7;
-const ll LINF = 1e18 + 7;
-const int MOD = 1e9 + 7;
+int n, m, k;
+int check(int i, int mx) {
+	return min(max(i, 0), mx);
+}
 
-void fast_io() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int pref(int i, int j) {
+	return sum[check(i, n)][check(j, m)];
 }
 
 void solve() {
-    int n, m, k; std::cin >> n >> m >> k;
-    std::vector<pii> free;
-    for (int i = 0; i < n; i++) {
-        std::vector<char> row(m); for (char& x: row) {{
-            std::cin >> x;
-            if (x == '.') {
-                free 
-            }
-        };
-
-    }
+	cin >> n >> m >> k; k--;
+	vector<string> mine(n);
+	int all_gold = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> mine[i];
+		for (int j = 0; j < m; j++) {
+			all_gold += (mine[i][j] == 'g');
+		}
+	}
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			sum[i + 1][j + 1] = sum[i + 1][j] + sum[i][j + 1] - sum[i][j] + (mine[i][j] == 'g');
+		}
+	}
+	int ans = all_gold;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (mine[i][j] == '.') {
+				int a = i - k, b = i + k + 1, c = j - k, d = j + k + 1;
+				ans = min(ans, pref(b, d) - pref(a, d) - pref(b, c) + pref(a, c));
+			}
+		}
+	}
+	cout << all_gold - ans << "\n";
 }
 
 int main() {
-    fast_io();
-    int t = 1;
-    // cin >> t;
-    while (t--) {
-        solve();
-    }
-    return 0;
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int t = 1;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
 }
