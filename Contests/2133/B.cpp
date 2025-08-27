ad49c2fef143e2550fd24b1a669b5e264612a2b6
Nelexstar <kbartlewski5@gmail.com>
2025-08-27 12:58:00 +0200

    B

---
+++ Contests/2133/B.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    int n; std::cin >> n;
+    std::vector<ll> arr(n); for (ll& x: arr) std::cin >> x;
+    ll cost = 0;
+    std::sort(ALL(arr));
+    for (int i = n-1; i >= 0; i-=2) {
+        cost += arr[i];
+    }
+    std::cout << cost << "\n";
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
