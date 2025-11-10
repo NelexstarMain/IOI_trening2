54dc4411fafb9ac460fd6732119da82c6497ef25
Nelexstar <kbartlewski5@gmail.com>
2025-11-10 10:52:12 +0100

    kjgkshdkljnz

---
+++ UVa/Tennis_Rounds.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    int N, i, j;
+    while (cin >> N >> i >> j) {
+        int round = 1;
+        while ((i + 1) / 2 != (j + 1) / 2) {
+            i = (i + 1) / 2;
+            j = (j + 1) / 2;
+            round++;
+        }
+        cout << round << '\n';
+    }
+}
+
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
