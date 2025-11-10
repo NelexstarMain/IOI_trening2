ec8b8b71d7b87aea62ec0c8e70810ba70e8a5950
Nelexstar <kbartlewski5@gmail.com>
2025-11-10 14:29:58 +0100

    skhfkesfh

--- CP4/Exercises/04.py
+++ CP4/Exercises/04.py
@@ -16,6 +16,25 @@ for char in t:
 words = [word.strip(".,!?") for word in t.split()]
 words.sort()
 
+words_count = {}
+
+for word in words:
+    if (word in words_count) == False:
+        words_count[word] = 1
+    else:
+        words_count[word] += 1
+
+maxs = -1
+maxword = ""
+
+for key in words_count.keys():
+    if words_count[key] > maxs:
+        maxs = words_count[key]
+        maxword = key
+
 if words:  
     print(words[0])
+
+print(maxword)
 print(vowelscount, consonantscount)
+
