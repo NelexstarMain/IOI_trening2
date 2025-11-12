1aab8ac578467d738f55d2572b6c6cb79a106542
Nelexstar <kbartlewski5@gmail.com>
2025-11-12 16:49:53 +0100

    wfsfbwkebf

--- Problemset/Szkopuł/IO/hanoi.cpp
+++ Problemset/Szkopuł/IO/hanoi.cpp
@@ -7,7 +7,7 @@ using ull = unsigned long long;
 using ld = long double;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
-using di = deque<int>; 
+
 
 #define F first
 #define S second
@@ -28,121 +28,110 @@ void fast_io() {
     cin.tie(NULL);
 }
 
-
-struct VectorComparator {
-    bool operator()(const pair<di, int>& a, const pair<di, int>& b) const {
-        return a.first[0] > b.first[0]; 
-    }
-};
-
 void solve() {
     int n, m; cin >> n >> m;
-    vector<di> a(m); 
+
+    vector<int> all_data;
+    vector<int> starts(m, 0);     
+    vector<int> element_count(m, 0);
+
     bool zeros = false;
     int zeros_index = -1;
+    int non_empty_count = 0;
+    
+    int current_data_offset = 0; 
+    
     for (int i = 0; i < m; i++) {
         int k; cin >> k;
         if (k == 0) {
             zeros = true;
             zeros_index = i;
         } else {
-            di v; 
+            non_empty_count++;
+            starts[i] = current_data_offset;
+            element_count[i] = k;
+            current_data_offset += k;
+            
             for (int j = 0; j < k; ++j) { 
                 int x; cin >> x;
-                v.push_back(x); 
+                all_data.push_back(x); 
             }
-            a[i] = v; 
         }
     }
 
-
+    if (non_empty_count <= 1 && zeros) {
+        cout << "0\n";
+        return;
+    }
 
     int lowest_pick = INF, lowest_pick_index = -1, secondlowest = INF;
 
     for (int i = 0; i < m; i++) {
-            if (!a[i].empty()) {
-                if (a[i][0] < lowest_pick ) {
+        if (element_count[i] > 0) {
+            int first_val = all_data[starts[i]];
+            if (first_val < lowest_pick ) {
                 secondlowest = lowest_pick;
-                lowest_pick = a[i][0];
+                lowest_pick = first_val;
                 lowest_pick_index = i;
-                } else if (a[i][0] < secondlowest) {
-                    secondlowest = a[i][0];
-                }
+            } else if (first_val < secondlowest) {
+                secondlowest = first_val;
             }
-            
         }
+    }
 
-        int lowerhighest = INF;
-        if (!a[lowest_pick_index].empty()) {
-            lowerhighest = a[lowest_pick_index].back();
-        }
+    int lowerhighest = INF;
+    if (lowest_pick_index != -1 && element_count[lowest_pick_index] > 0) {
+        lowerhighest = all_data[starts[lowest_pick_index] + element_count[lowest_pick_index] - 1];
+    }
 
     vector<pii> moves;
     if (lowerhighest > secondlowest && !zeros) {
         cout << "-1\n";
         return;
     } else {
-
-        priority_queue<pair<di, int>, vector<pair<di, int>>, VectorComparator> minHeap;
-        priority_queue<pii, vector<pii>, greater<pii>> singleHeap;
-        
+        priority_queue<pair<int, pii>, vector<pair<int, pii>>, greater<pair<int, pii>>> minHeap;
+        vector<int> processed_count(m, 0); 
+        bool used = false;
         if (lowerhighest > secondlowest && zeros) {
             moves.PB({lowest_pick_index + 1, zeros_index + 1});
-            a[lowest_pick_index].pop_front();
-            
-
+            processed_count[lowest_pick_index]++; 
+        } else {
+            zeros_index = lowest_pick_index;
+            used = true;
         }
-
 
         for (int i = 0; i < m; i++) {
-            if (!a[i].empty() && a[i].size() > 1) {
-                minHeap.push({a[i], i});
-            } else if (!a[i].empty() && a[i].size() == 1) {
-                singleHeap.push({a[i][0], i});
+            if (element_count[i] > 0 && processed_count[i] < element_count[i]) {
+                if (used && i == zeros_index) {
+                    continue;
+                }
+                int current_j = processed_count[i];
+                int val = all_data[starts[i] + current_j];
+                minHeap.push({val, {i, current_j}});
             }
         }
 
-        while (!minHeap.empty() || !singleHeap.empty()) {
-            if (singleHeap.empty()) {
-                auto top = minHeap.top();
-                di vec = top.first; 
-                int idx = top.second;        
-                minHeap.pop();
-
-                moves.PB({idx + 1, zeros_index + 1});
-
-                vec.pop_front();
-                if (!vec.empty()) minHeap.push({vec, idx});
-            } else if (minHeap.empty()) {
-                auto top = singleHeap.top();
-                int val = top.first; 
-                int idx = top.second;        
-                singleHeap.pop();
-
-                moves.PB({idx + 1, zeros_index + 1});
-            } else {
-                auto top1 = minHeap.top();
-                auto top2 = singleHeap.top();
-                int val2 = top2.first;
-                di vec = top1.first; 
-                int idx = top1.second;     
-                int idx2 = top2.second;   
-
-                minHeap.pop();
-                singleHeap.pop();
-
-                if (vec[0] < val2) {
-                    moves.PB({idx + 1, zeros_index + 1});
-                    vec.pop_front();
-                    a[idx] = vec;
-                    if (!vec.empty()) minHeap.push({vec, idx});
-                } else {
-                    moves.PB({idx2 + 1, zeros_index + 1});
-                }
-  
+        while (!minHeap.empty()) {
+            auto top = minHeap.top();
+            minHeap.pop();
+            
+            int idx_a = top.second.first;
+            int idx_j = top.second.second;
+            if (idx_j != processed_count[idx_a]) {
+                continue; 
             }
             
+            moves.PB({idx_a + 1, zeros_index + 1});
+            processed_count[idx_a]++; 
+
+            int next_j = processed_count[idx_a];
+
+            if (next_j < element_count[idx_a]) {
+                int next_val = all_data[starts[idx_a] + next_j];
+                minHeap.push({next_val, {idx_a, next_j}});
+            }
         }
+        
         std::cout << SIZE(moves) << "\n";
         for (auto move : moves) {
             std::cout << move.F << " " << move.S << "\n";
