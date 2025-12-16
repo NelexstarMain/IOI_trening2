ce844e7e0076a5e5d56fdf13536daf1b63f82c92
NelexstarMain <kbartlewski5@gmail.com>
2025-12-16 14:21:15 +0100

    wAZ81fqCVs00HMa

--- CP4/UVa/10906.cpp
+++ CP4/UVa/10906.cpp
@@ -2,65 +2,47 @@
 
 using namespace std;
 
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
-
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i, a, b) for(int i = a; i <= b; ++i)
-#define FWD(i, a, b) for(int i = a; i < b; ++i)
-#define REV(i, a, b) for(int i = a; i >= b; --i)
-#define ALL(a) (a).begin(), (a).end()
-#define SIZE(a) (int)((a).size())
-
-const int INF = 1e9 + 7;
-const ll LINF = 1e18 + 7;
-const int MOD = 1e9 + 7;
-
-void fast_io() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-}
-
-// 2
-// 2
-// A=2+3
-// B=A+A
-// 3
-// A=2+3
-// B=A+4
-// C=B+5
-
-void solve() {
-    int n; std::cin >> n;
-    std::map ver;
-    std::string line;
-    while (std::getline(std::cin, line)) {
-        std::stringstream ss(line);
-        std::string name, trash, curr; ss >> name;
-        ss >> trash;
-        std::vector<std::string> all;
-        while (ss >> curr) all.PB(curr);
-        std::string res = "";
-        std::string before = all[0];
-        for (int i = 1; i < SIZE(all); i++) {
-
-        }
-
-    }
-}
-
 int main() {
-    fast_io();
-    int t = 1;
-    if(!(cin >> t)) return 0;
-    while (t--) {
-        solve();
+
+
+    int n;
+    cin >> n;
+    for (int i = 1; i < n + 1; i++) {
+        map<string, pair<char, string>> variables;
+        int m;
+        cin >> m;
+        string varC, varA, varB, tmp;
+        char op;
+        while (m--) {
+            cin >> varC >> tmp >> varA >> op >> varB;
+            string expr;
+            if (variables.count(varA)) {
+                if (variables[varA].first == '+' && op == '*') {
+                    expr += '(' + variables[varA].second + ')';
+                } else {
+                    expr += variables[varA].second;
+                }
+            } else {
+                expr += varA;
+                variables[varA] = {'$', varA};
+            }
+            expr += op;
+            if (variables.count(varB)) {
+                if (variables[varB].first == '*' && op == '+' || variables[varB].first == '$') {
+                    expr += variables[varB].second;
+                } else {
+                    expr += '(' + variables[varB].second + ')';
+                }
+            } else {
+                expr += varB;
+                variables[varB] = {'$', varB};
+            }
+            variables[varC] = {op, expr};
+        }
+        cout << "Expression #" << i << ": " << variables[varC].second << endl;
     }
+
+
+    cerr << "time taken : " << (float) clock() / CLOCKS_PER_SEC << " secs" << endl;
     return 0;
 }
