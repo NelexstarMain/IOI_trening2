f4394403eb2c0779226d9d4904d69e33411ded2f
NelexstarMain <kbartlewski5@gmail.com>
2025-12-19 11:49:14 +0100

    lQzHKHsDVGT9Mjj

---
+++ CP4/Kattis/BaloniV.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    int n; std::cin >> n;
+    std::vector<int> arows(100005, 0); 
+    int r = 0;
+    for (int i = 0; i < n; i++) {
+        int c; std::cin >> c;
+        if (arows[c] > 0) {
+            arows[c]--;
+            if (c-1 > 0) arows[c-1]++;
+        } else {
+            r++;
+            arows[c-1]++;
+        }
+    }
+    std::cout << r << "\n";
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
