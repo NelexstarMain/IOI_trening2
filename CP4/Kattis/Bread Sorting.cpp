2905f4d975fc3f35a09f70adbbf4de5d1118082f
NelexstarMain <kbartlewski5@gmail.com>
2026-01-12 10:21:31 +0100

    DUCXiiiNbsLn1UP

---
+++ CP4/Kattis/Bread Sorting.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+int inver(std::vector<int>& a) {
+    
+}
+
+void solve() {
+    int n; std::cin >> n;
+    std::vector<int> a(n), b(n); 
+    for (int& x: a) std::cin >> x;
+    for (int& x: b) std::cin >> x;
+
+}
+
+int main() {
+    fast_io();
+    int t = 1;
+    if(!(cin >> t)) return 0;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
