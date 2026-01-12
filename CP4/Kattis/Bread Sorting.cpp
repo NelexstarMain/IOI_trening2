2960c7d6b746f23be0b053603014edf60a705a2f
NelexstarMain <kbartlewski5@gmail.com>
2026-01-12 14:50:42 +0100

    0YEhXgAHaZQY5Np

--- CP4/Kattis/Bread Sorting.cpp
+++ CP4/Kattis/Bread Sorting.cpp
@@ -1,50 +1,63 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-using ll = long long;
-using ull = unsigned long long;
-using ld = long double;
-using pii = pair<int, int>;
-using pll = pair<ll, ll>;
+typedef long long ll;
 
-#define F first
-#define S second
-#define PB push_back
-#define MP make_pair
-#define REP(i, a, b) for(int i = a; i <= b; ++i)
-#define FWD(i, a, b) for(int i = a; i < b; ++i)
-#define REV(i, a, b) for(int i = a; i >= b; --i)
-#define ALL(a) (a).begin(), (a).end()
-#define SIZE(a) (int)((a).size())
+ll merge_and_count(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
 
-const int INF = 1e9 + 7;
-const ll LINF = 1e18 + 7;
-const int MOD = 1e9 + 7;
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
 
-void fast_io() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    int i = 0, j = 0, k = left;
+    ll inv_count = 0;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+            inv_count += (n1 - i);
+        }
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+
+    return inv_count;
 }
 
-int inver(std::vector<int>& a) {
-    
+ll count_inversions(vector<int>& arr, int left, int right) {
+    ll count = 0;
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        count += count_inversions(arr, left, mid);
+        count += count_inversions(arr, mid + 1, right);
+        count += merge_and_count(arr, left, mid, right);
+    }
+    return count;
 }
 
 void solve() {
-    int n; std::cin >> n;
-    std::vector<int> a(n), b(n); 
-    for (int& x: a) std::cin >> x;
-    for (int& x: b) std::cin >> x;
+    int n; 
+    if (!(cin >> n)) return;
+    vector<int> a(n), b(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < n; i++) cin >> b[i];
 
+    ll inv_a = count_inversions(a, 0, n - 1);
+    ll inv_b = count_inversions(b, 0, n - 1);
+    if (inv_a % 2 == inv_b % 2) {
+        cout << "Possible" << endl;
+    } else {
+        cout << "Impossible" << endl;
+    }
 }
 
 int main() {
-    fast_io();
-    int t = 1;
-    if(!(cin >> t)) return 0;
-    while (t--) {
-        solve();
-    }
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    solve();
     return 0;
 }
