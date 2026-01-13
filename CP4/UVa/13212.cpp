cd9831417883d16965e1b84a364a0b0af32beddb
NelexstarMain <kbartlewski5@gmail.com>
2026-01-13 11:00:23 +0100

    O0pj3X9INDcxgya

--- CP4/UVa/13212.cpp
+++ CP4/UVa/13212.cpp
@@ -27,16 +27,57 @@ void fast_io() {
     cin.tie(NULL);
 }
 
+ll merge_and_count(vector<int>& arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    vector<int> L(n1), R(n2);
+
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    ll inv_count = 0;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k++] = L[i++];
+        } else {
+            arr[k++] = R[j++];
+            inv_count += (n1 - i);
+        }
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+
+    return inv_count;
+}
+
+ll count_inversions(vector<int>& arr, int left, int right) {
+    ll count = 0;
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        count += count_inversions(arr, left, mid);
+        count += count_inversions(arr, mid + 1, right);
+        count += merge_and_count(arr, left, mid, right);
+    }
+    return count;
+}
+
 void solve() {
-    
+    int n; 
+    while (std::cin >> n && n > 0) {
+        std::vector<int> a;
+        for (int i = 0; i < n; i++) {
+            int c; std::cin >> c;
+            a.PB(c);
+        }
+        ll inv = count_inversions(a, 0, SIZE(a)-1);
+        std::cout << inv << "\n";
+    }
 }
 
 int main() {
     fast_io();
-    int t = 1;
-    if(!(cin >> t)) return 0;
-    while (t--) {
-        solve();
-    }
+    solve();
     return 0;
 }
