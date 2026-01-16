d963b65e89135df5645af8288d89c5f21bb995bf
NelexstarMain <kbartlewski5@gmail.com>
2026-01-16 10:50:42 +0100

    RvUsZCj78ikuwsu

---
+++ CP4/2_Data_Structures/1_library_linear/h_bit_manipulation/uva/11933 - Splitting Numbers.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    int n; 
+    while (std::cin >> n && n) {
+        unsigned int a = 0, temp = n;
+        bool bit_to_a = true;
+
+        while (temp) {
+            unsigned int lsb = temp & -temp;
+            if (bit_to_a) a |= lsb;        
+            bit_to_a = !bit_to_a;
+            temp ^= lsb;                    
+        }
+        std::cout << a << " " << (n ^ a) << "\n"; 
+    }
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
