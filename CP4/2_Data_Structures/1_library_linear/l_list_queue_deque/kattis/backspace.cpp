0832b7e92290718f8a9b10d45318a067bbc9879a
NelexstarMain <kbartlewski5@gmail.com>
2026-01-27 11:20:03 +0100

    zIFDvHgLPUjIMPL

---
+++ CP4/2_Data_Structures/1_library_linear/l_list_queue_deque/kattis/backspace.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    std::string s, A; std::cin >> s;
+    for (char x: s) {
+        if (x == '<') {
+            if (SIZE(A) > 0) A.pop_back();
+        } else {
+            A += x;
+        }
+    }
+    // for (int i = 0; i < index; i++) std::cout << A[i];
+    for (char x: A) std::cout << x;
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
