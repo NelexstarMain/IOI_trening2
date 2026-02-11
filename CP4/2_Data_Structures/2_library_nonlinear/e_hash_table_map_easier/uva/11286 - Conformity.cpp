53448f7aa7b0e17c980cf29209abb99b2396630e
NelexstarMain <kbartlewski5@gmail.com>
2026-02-11 09:16:44 +0100

    aHNNZRfCVYSdL92

---
+++ CP4/2_Data_Structures/2_library_nonlinear/e_hash_table_map_easier/uva/11286 - Conformity.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    int n;
+    while (std::cin >> n && n > 0) {
+        std::map<std::vector<int>, int> A;
+        int m = 0;
+        for (int i = 0; i < n; i++) {
+            std::vector<int> a(5); for (int& x: a) std::cin >> x;
+            std::sort(ALL(a));
+            A[a]++; m = std::max(m, A[a]);
+        }
+        int c = 0;
+        for (auto [k, v]: A) if (v == m) c += v;
+        std::cout << c << "\n";
+    }
+    
+
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
