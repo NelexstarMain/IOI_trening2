6182fa6a8d1fa80dcfa1c84af986454ab88aede7
NelexstarMain <kbartlewski5@gmail.com>
2026-02-17 13:32:04 +0100

    8i9PtZdh025RQy8

---
+++ SNIPPET.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    
+}
+
+int main() {
+    fast_io();
+    int T; std::cin >> T;
+    std::cin.ingore();
+    while (T--) solve();
+    return 0;
+}
