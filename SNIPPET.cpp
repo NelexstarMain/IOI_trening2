53f95520984db8633911209bbb1fcd36f39c6332
NelexstarMain <kbartlewski5@gmail.com>
2026-02-17 14:47:27 +0100

    AhhcWnedGAr1vxN

--- SNIPPET.cpp
+++ SNIPPET.cpp
@@ -21,13 +21,18 @@ void fast_io() {
 }
 
 void solve() {
-    
+    int seed, seed2; std::cin >> seed >> seed2;
+    std::string s; s.reserve(10e5+7); s = "0"; int c = 1;
+    for (int i = 1; i < 10e4; i++) { if ((seed&(seed2*i)) != 0) c = i%(seed&(seed2*i)); if (c != 0) s += (i%c + '0'); }
+    std::cout << s << "\n";
 }
 
 int main() {
     fast_io();
     int T; std::cin >> T;
-    std::cin.ingore();
+    std::cin.ignore();
     while (T--) solve();
+    std::cerr << "time taken : " << (double) clock() / CLOCKS_PER_SEC << " secs" << std::endl;
+    
     return 0;
 }
