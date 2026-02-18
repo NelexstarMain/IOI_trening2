32f89c85f6adb18a784c0b7ad404792aa62ebc41
NelexstarMain <kbartlewski5@gmail.com>
2026-02-18 08:58:45 +0100

    wsWdBJg9u2qSNAu

---
+++ CP4/2_Data_Structures/2_library_nonlinear/g_balanced_tree_set/kattis/candydivision.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    ll n; std::cin >> n;
+    std::set<ll> A;
+    for (ll i = 1; i*i <= n; i++) if (n%i==0) { A.insert(i-1); A.insert((n/i)-1); }
+    for (ll x: A) std::cout << x << " ";
+    std::cout << "\n";
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
