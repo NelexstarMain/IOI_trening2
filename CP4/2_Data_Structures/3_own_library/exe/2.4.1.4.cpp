eb2a094cf452a7514d04f98bffeb670d4e161b01
NelexstarMain <kbartlewski5@gmail.com>
2026-02-25 12:00:10 +0100

    FupgxSsBLMok2XV

--- CP4/2_Data_Structures/3_own_library/exe/2.4.1.4.cpp
+++ CP4/2_Data_Structures/3_own_library/exe/2.4.1.4.cpp
@@ -22,13 +22,81 @@ const int INF = 1e9 + 7;
 const ll LINF = 1e18 + 7;
 const int MOD = 1e9 + 7;
 
+using edge = std::tuple<int, int, int>; 
+using AdjacencyMatrix = std::vector<std::vector<int>>;
+using AdjacencyList   = std::vector<std::vector<pii>>;
+using EdgeList        = std::vector<edge>;
+
 void fast_io() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 }
 
+
+AdjacencyMatrix AM(V, std::vector<int>(V, 0));
+AdjacencyList AL(V);
+EdgeList EL;
+
+
+EdgeList AM_EL(AdjacencyMatrix& A) {
+    int V = SIZE(A);
+    EdgeList NEW;
+    FWD(i, 0, V) {
+        FWD(j, 0, V) {
+            if (A[i][j] != 0) NEW.PB({i, j, A[i][j]});
+        }
+    }
+    return NEW;
+}
+
+
+EdgeList AL_EL(AdjacencyList& A) {
+    int V = SIZE(A);
+    EdgeList NEW;
+    FWD(u, 0, V) {
+        for (auto &e : A[u]) {
+            NEW.PB({u, e.F, e.S});
+        }
+    }
+    return NEW;
+}
+
+
+AdjacencyList EL_AL(EdgeList& A, int V) {
+    AdjacencyList NEW(V);
+    for (auto &e : A) {
+        auto [u, v, w] = e;
+        NEW[u].PB({v, w});
+    }
+    return NEW;
+}
+
+
+AdjacencyMatrix EL_AM(EdgeList& A, int V) {
+    AdjacencyMatrix NEW(V, vector<int>(V, 0));
+    for (auto &e : A) {
+        auto [u, v, w] = e;
+        NEW[u][v] = w;
+    }
+    return NEW;
+}
+
+
+AdjacencyMatrix AL_AM(AdjacencyList& A) {
+    int V = SIZE(A);
+    AdjacencyMatrix NEW(V, vector<int>(V, 0));
+    FWD(i, 0, V) {
+        for (auto &curr : A[i]) {
+            NEW[i][curr.F] = curr.S;
+        }
+    }
+    return NEW;
+}
+
+
 void solve() {
-    
+    //AM->AL AL->AM AM->EL El->AM AL->EL El->AL
+
 }
 
 int main() {
