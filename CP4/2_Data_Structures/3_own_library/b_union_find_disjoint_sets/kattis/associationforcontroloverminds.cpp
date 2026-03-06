669701fcd83e41a7e21f32633e3035383ff41535
NelexstarMain <kbartlewski5@gmail.com>
2026-03-06 09:43:05 +0100

    cQRi4y2WPQzI7rb

---
+++ CP4/2_Data_Structures/3_own_library/b_union_find_disjoint_sets/kattis/associationforcontroloverminds.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+struct UnionFind {
+    public:
+        vector<int> p, rank, setsSizes;
+        int numsets;
+
+        UnionFind(int n) {
+            p.assign(n, 0); for (int i=0; i<n; i++) p[i] = i;
+            rank.assign(n, 0);            
+            setsSizes.assign(n, 1);
+            numsets = n;
+        }
+
+        int findSet(int m) {
+            // idziemy po rodzicu do góry dodajemy tez indexy pokolei do korzeniaj potem 
+            // jak znajdziemy rodzica to wszyskie podłanczamy do korzenia
+            if (m == p[m]) return p[m];
+            return p[m] = findSet(p[m]); // nelek tu był 
+        }
+        bool sameSet(int i, int j) { return ( findSet(i) == findSet(j)); }
+
+        void UnionSet(int i, int j) {
+            int x = findSet(i), y = findSet(j); 
+            if (x == y) return;
+            if (rank[x] > rank[y]) swap(x, y); // belek tu był
+            p[x] = y;
+            setsSizes[y] += setsSizes[x];
+            rank[y] = std::max(rank[x]+1, rank[y]); // NELEK 
+            numsets--;
+        }
+};
+
+
+void solve() {
+    int n; cin >> n;
+    UnionFind UF(5*10e5+1);
+    int count = 0;
+    for (int i=0; i<n; i++) {
+        int m; cin >> m;
+        set<int> A;
+        for (int j=0; j<m; j++) {
+            int curr; cin >> curr;
+            A.insert(UF.findSet(curr));
+        }
+        int s=0; for (int x: A) s+=UF.setsSizes[x];
+        if (s == m) {
+            count++;
+            int first = *A.begin();
+            A.erase(A.begin());
+            for (int x: A) {
+                UF.UnionSet(first, x);
+            }
+        }
+    }
+    cout << count << "\n";
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
