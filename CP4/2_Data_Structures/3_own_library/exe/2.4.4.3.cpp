15929c2df77408f6ae4f14ca7f38117b1f480ae9
NelexstarMain <kbartlewski5@gmail.com>
2026-03-23 09:35:15 +0100

    vtUf0qp7Vhl37Lw

---
+++ CP4/2_Data_Structures/3_own_library/exe/2.4.4.3.cpp
@@ -0,0 +1,15 @@
+2.4.4.3
+musimy zrobić w zasadzie to samo zamiast robić min robimy sume += 
+czyli jak mamy:
+
+
+1 2 3 4 5 6 7 8
+
+to drzewo bedzie
+
+      46
+  10       36
+ 3   7   11  15
+1 2 3 4 5 6 7  8
+
+jak robimy lazy propagation i jak robimy update itd to poprostu zamieniamy operacjie na sume
