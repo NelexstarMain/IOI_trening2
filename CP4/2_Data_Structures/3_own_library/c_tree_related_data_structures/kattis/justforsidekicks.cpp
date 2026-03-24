7e3d0394161a5c46c9de9d99453e644903edfff8
NelexstarMain <kbartlewski5@gmail.com>
2026-03-24 09:19:26 +0100

    ZvLRBKtltdEMadz

---
+++ CP4/2_Data_Structures/3_own_library/c_tree_related_data_structures/kattis/justforsidekicks.cpp
@@ -0,0 +1,128 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull= unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vll = vector<ll>;
+using vi = vector<int>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+#define LSOne(S) ((S) & (-S))
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+class FenwickTree {
+    private:
+        vll ft;
+    public:
+        FenwickTree() {}
+        FenwickTree(int m) {
+            ft.assign(m+1, 0);
+        }
+        void build(const vll &f) {
+            int m = SIZE(f)-1;
+            ft.assign(m+1, 0);
+            for (int i=1; i<=m; i++) {
+                ft[i] += f[i];
+                if (i+LSOne(i) <= m) {
+                    ft[i+LSOne(i)] += ft[i];
+                }
+            }
+        }
+        FenwickTree(const vll &f) { build(f); }
+        
+        FenwickTree(int m, const vi &s) {
+            vll f(m+1, 0);
+            for (int x: s) {
+                f[x]++;
+            }
+            build(f);
+        }
+        ll rsq(int j) {
+            ll sum = 0;
+            for (; j; j-=LSOne(j)) {
+                sum += ft[j];
+            }
+            return sum;
+        }
+        ll rsq(int i, int j) { return (rsq(j) - rsq(i-1)); }
+
+        void update(int i, ll v) {
+            for (; i < SIZE(ft); i += LSOne(i)) {
+                ft[i] += v;
+            }
+        }
+        ll select(ll k) {
+            int p = 1;
+            while (p*2 < SIZE(ft)) p*=2;
+            int i = 0;
+            while (p) {
+                if (i+p < SIZE(ft) && k > ft[i+p]) {
+                    i+=p;
+                    k-=ft[i];
+                }
+                p/=2;
+            }
+            return i+1;
+        }
+};
+
+int tonum(char a) {
+    return (a-'0');
+}
+
+void solve() {
+    int n, q; cin >> n >> q;
+    vi G(6, 0); for (int &x: G) cin >> x; 
+    vector<FenwickTree> T(6, FenwickTree(n));
+    string S; cin >> S;
+    for (int i=1; i<=n; i++) {
+        int num = tonum(S[i-1])-1;
+        T[num].update(i, 1);
+    }
+    for (int i=0; i<q; i++) {
+        int act; cin >> act;
+        if (act == 1) {
+            int k, p; cin >> k >> p; p--;
+            int kn = tonum(S[k-1])-1;
+            T[kn].update(k, -1); T[p].update(k, 1);
+            S[k-1] = (char)((p+1)+'0');
+        } else if (act == 2) {
+            int p, v; cin >> p >> v; p--;
+            G[p] = v;
+        } else {
+            int l, r; cin >> l >> r;
+            ll S = 0;
+            for (int j=0; j<6; j++) {
+                S += (ll)G[j]*T[j].rsq(l, r);
+            }
+            cout << S << "\n";
+        }
+    }
+}
+
+int main() {
+    fast_io();
+    solve();
+    return 0;
+}
