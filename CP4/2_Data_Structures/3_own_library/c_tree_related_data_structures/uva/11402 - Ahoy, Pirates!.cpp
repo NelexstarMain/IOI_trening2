050ed3a77e51059d9ea360282c83659144a46cd2
NelexstarMain <kbartlewski5@gmail.com>
2026-03-25 14:12:38 +0100

    G8EUkAnJFzJ9yFy

---
+++ CP4/2_Data_Structures/3_own_library/c_tree_related_data_structures/uva/11402 - Ahoy, Pirates!.cpp
@@ -0,0 +1,181 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vi = vector<int>;
+
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define REP(i, a, b) for(int i = a; i <= b; ++i)
+#define FWD(i, a, b) for(int i = a; i < b; ++i)
+#define REV(i, a, b) for(int i = a; i >= b; --i)
+#define ALL(a) (a).begin(), (a).end()
+#define SIZE(a) (int)((a).size())
+
+const int INF = 1e9 + 7;
+const ll LINF = 1e18 + 7;
+const int MOD = 1e9 + 7;
+
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+class SegmentTree {
+    private:
+        int n;
+        vi A, st, lazy, flip;
+        int l(int i) { return (i<<1); }
+        int r(int i) { return (i<<1)+1; }
+
+        int counquer(int a, int b) {
+            return a+b;
+        }
+
+        void build(int p, int L, int R) {
+            if (L == R) {
+                st[p] = A[L];
+            } else {
+                int m = (R+L)/2;
+                build(l(p), L, m);
+                build(r(p), m+1, R);
+                st[p] += counquer(st[l(p)], st[r(p)]);
+            }
+        }
+
+        void propagate(int p, int L, int R) {
+            if (lazy[p] != -1) {
+                st[p] = lazy[p]*(R-L+1);
+                if (L != R) {
+                    lazy[l(p)] = lazy[r(p)] = lazy[p];
+                    flip[l(p)] = flip[r(p)] = -1;
+                } else {
+                    A[L] = lazy[p];
+                }
+                lazy[p] = -1;
+            } 
+        }
+
+        void flipoperation(int p, int L, int R) {
+            if (flip[p] != -1) {
+                st[p] = (R-L-st[p]+1);
+                if (L != R) {
+                    if (lazy[l(p)] != -1) lazy[l(p)] = 1-lazy[l(p)];
+                    else flip[l(p)] = (flip[l(p)] == 1)? -1 : 1;
+
+                    if (lazy[r(p)] != -1) lazy[r(p)] = 1-lazy[r(p)];
+                    else flip[r(p)] = (flip[r(p)] == 1)? -1 : 1;
+                } else {
+                    A[L] = st[p];            
+                }
+                flip[p] = -1; 
+            }
+        }
+
+        int RSQ(int p, int L, int R, int i, int j) {
+            propagate(p, L, R);
+            flipoperation(p, L, R);
+            if (i > j) return 0;
+            if ((i<=L) && (j>=R)) return st[p];
+            else {
+                int m = (L+R)/2;
+                return counquer(RSQ(l(p), L, m, i, min(m, j)), RSQ(r(p), m+1, R, max(m+1, i), j));
+            }
+        }
+
+        void update(int p, int L, int R, int i, int j, int val) {
+            propagate(p, L, R);
+            flipoperation(p, L, R);
+            if (i > j) return;
+            if ((i<=L) && (R<=j)) {
+                lazy[p] = val;
+                propagate(p, L, R);
+            } else {
+                int m = (R+L)/2;
+                update(l(p), L, m, i, min(j, m), val);
+                update(r(p), m+1, R, max(i, m+1), j, val);
+                // int lsubtree = (lazy[l(p)] != -1) ? lazy[l(p)]*(m-L+1): st[l(p)];
+                // int rsubtree = (lazy[r(p)] != -1) ? lazy[r(p)]*(R-m): st[r(p)];
+                propagate(p, L, R);
+                st[p] = counquer(st[l(p)], st[r(p)]);
+            }
+        }
+        void flips(int p, int L, int R, int i, int j) {
+            propagate(p, L, R);
+            flipoperation(p, L, R);
+            if (i > j) return;
+            if ((i<=L) && (R<=j)) {
+                flip[p] = 1;
+                flipoperation(p, L, R);
+            } else {
+                int m = (L+R)/2;
+                flips(l(p), L, m, i, min(j, m));
+                flips(r(p), m+1, R, max(i, m+1), j);
+                flipoperation(p, L, R);
+                st[p] = counquer(st[l(p)], st[r(p)]);
+            }
+        }
+
+    public:
+        SegmentTree(int sz) : n(sz), st(4*n, 0), lazy(4*n, -1), flip(4*n, -1) {}
+
+        SegmentTree(const vi& initialA) : SegmentTree(SIZE(initialA)) {
+            A = initialA;
+            build(1, 0, n-1);
+        }
+
+        void update(int i, int j, int val) { update(1, 0, n-1, i, j, val); }
+
+        int RSQ(int i, int j) { return RSQ(1, 0, n-1, i, j); }
+        
+        void flips(int i, int j) { flips(1, 0, n-1, i, j); }
+
+};
+
+int sbpow(int n) {
+    return (int) pow(2, ceil(log2(n)));
+} 
+
+void solve(int z) {
+    string P; P.reserve(16777216);
+    int N; cin >> N;
+    for (int i=0; i<N; i++) {
+        int C; string Part; cin >> C >> Part;
+        while (C--) P+=Part;
+    }
+    P.shrink_to_fit();
+    int S = sbpow(SIZE(P));
+    vi A; A.reserve(S);
+    // cout << P << "\n";
+    for (char x: P) A.PB((x-'0'));
+    for (int i=SIZE(A); i<S; i++) A.PB(0);
+    // for (int x: A) cout << x << " ";
+    // cout << "\n";
+    SegmentTree St(A);
+    cout << "Case " << z << ":\n";
+    int Q; cin >> Q;
+    int q = 1;
+    for (int i=0; i<Q; i++) {
+        char action; cin >> action;
+        int x, y; cin >> x >> y;
+        if (action == 'F') St.update(x, y, 1);
+        else if (action == 'E') St.update(x, y, 0);
+        else if (action == 'I') St.flips(x, y);
+        else cout << "Q" << q++ << ": " << St.RSQ(x, y) << "\n";
+    }
+}
+
+int main() {
+    fast_io();
+    int T; cin >> T;
+    int z = 1;
+    while (T--) solve(z++);
+    return 0;
+}
--- CP4/2_Data_Structures/3_own_library/c_tree_related_data_structures/uva/test.cpp
+++ CP4/2_Data_Structures/3_own_library/c_tree_related_data_structures/uva/test.cpp
@@ -8,6 +8,7 @@ using ld = long double;
 using pii = pair<int, int>;
 using pll = pair<ll, ll>;
 using vi = vector<int>;
+
 #define F first
 #define S second
 #define PB push_back
@@ -30,14 +31,12 @@ void fast_io() {
 class SegmentTree {
     private:
         int n;
-        vi A, st, lazy;
+        vi A, st, lazy, flip;
         int l(int i) { return (i<<1); }
         int r(int i) { return (i<<1)+1; }
 
         int counquer(int a, int b) {
-            if (a == -1) return b;
-            if (b == -1) return a;
-            return min(a, b);
+            return a+b;
         }
 
         void build(int p, int L, int R) {
@@ -47,13 +46,13 @@ class SegmentTree {
                 int m = (R+L)/2;
                 build(l(p), L, m);
                 build(r(p), m+1, R);
-                st[p] = counquer(st[l(p)], st[r(p)]);
+                st[p] += counquer(st[l(p)], st[r(p)]);
             }
         }
 
         void propagate(int p, int L, int R) {
             if (lazy[p] != -1) {
-                st[p] = lazy[p];
+                st[p] = lazy[p]*(R-L+1);
                 if (L != R) {
                     lazy[l(p)] = lazy[r(p)] = lazy[p];
                 } else {
@@ -63,18 +62,32 @@ class SegmentTree {
             } 
         }
 
-        int RMQ(int p, int L, int R, int i, int j) {
+        void flipoperation(int p, int L, int R) {
+            if (flip[p] != -1) {
+                st[p] = (R-L-st[p]+1);
+                if (L != R) {
+                    flip[l(p)] = flip[r(p)] = 1;
+                } else {
+                    A[L] = (R-L-st[p]+1);               
+                }
+                flip[p] = -1; 
+            }
+        }
+
+        int RSQ(int p, int L, int R, int i, int j) {
             propagate(p, L, R);
-            if (i > j) return -1;
+            flipoperation(p, L, R);
+            if (i > j) return 0;
             if ((i<=L) && (j>=R)) return st[p];
             else {
                 int m = (L+R)/2;
-                return counquer(RMQ(l(p), L, m, i, min(m, j)), RMQ(r(p), m+1, R, max(m+1, i), j));
+                return counquer(RSQ(l(p), L, m, i, min(m, j)), RSQ(r(p), m+1, R, max(m+1, i), j));
             }
         }
 
         void update(int p, int L, int R, int i, int j, int val) {
             propagate(p, L, R);
+            flipoperation(p, L, R);
             if (i > j) return;
             if ((i<=L) && (R<=j)) {
                 lazy[p] = val;
@@ -83,14 +96,30 @@ class SegmentTree {
                 int m = (R+L)/2;
                 update(l(p), L, m, i, min(j, m), val);
                 update(r(p), m+1, R, max(i, m+1), j, val);
-                int lsubtree = (lazy[l(p)] != -1) ? lazy[l(p)]: st[l(p)];
-                int rsubtree = (lazy[r(p)] != -1) ? lazy[r(p)]: st[r(p)];
-                st[p] = (lsubtree <= rsubtree) ? st[l(p)]: st[r(p)];
+                // int lsubtree = (lazy[l(p)] != -1) ? lazy[l(p)]*(m-L+1): st[l(p)];
+                // int rsubtree = (lazy[r(p)] != -1) ? lazy[r(p)]*(R-m): st[r(p)];
+                propagate(p, L, R);
+                st[p] = counquer(st[l(p)], st[r(p)]);
+            }
+        }
+        void flips(int p, int L, int R, int i, int j) {
+            propagate(p, L, R);
+            flipoperation(p, L, R);
+            if (i > j) return;
+            if ((i<=L) && (R<=j)) {
+                flip[p] = 1;
+                flipoperation(p, L, R);
+            } else {
+                int m = (L+R)/2;
+                flips(l(p), L, m, i, min(j, m));
+                flips(r(p), m+1, R, max(i, m+1), j);
+                flipoperation(p, L, R);
+                st[p] = counquer(st[l(p)], st[r(p)]);
             }
         }
 
     public:
-        SegmentTree(int sz) : n(sz), st(4*n), lazy(4*n, -1) {}
+        SegmentTree(int sz) : n(sz), st(4*n, 0), lazy(4*n, -1), flip(4*n, -1) {}
 
         SegmentTree(const vi& initialA) : SegmentTree(SIZE(initialA)) {
             A = initialA;
@@ -99,16 +128,25 @@ class SegmentTree {
 
         void update(int i, int j, int val) { update(1, 0, n-1, i, j, val); }
 
-        int RMQ(int i, int j) { return RMQ(1, 0, n-1, i, j); }
+        int RSQ(int i, int j) { return RSQ(1, 0, n-1, i, j); }
+        
+        void flips(int i, int j) { flips(1, 0, n-1, i, j); }
 
 };
 
+int sbpow(int n) {
+    return (int) pow(2, ceil(log2(n)));
+} 
+
 void solve() {
-    //nothing
+    vi A = {1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    SegmentTree St(A);
+    St.flips(0, 1);
+    cout << St.RSQ(2,2);
+
 }
 
 int main() {
-    fast_io();
     solve();
     return 0;
 }
